a97b8a9d9f41c74d3f2b58195635aa8a8248cabe
kanchan0518 <itskanch@gmail.com>
2024-12-12 08:49:49 +0530

    cors error

--- music-system-backend/index.js
+++ music-system-backend/index.js
@@ -2,34 +2,25 @@ require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+
 const app = express();
 
 // Import Routes
 const userRoutes = require("./routes/user");
 const playList = require("./routes/playlists");
-const PORT = 8000;
 const CONNECTION_URL = process.env.MONGO_URI;
 
 app.use(cors());
-
-
 app.use(express.json());
-
-
 app.use("/user", userRoutes);
-app.use('/playlist', playList);
+app.use("/playlist", playList);
 
 app.get("/", (req, res) => {
-    res.send({ message: "Express Server for Music System is running on vercel" });
+    res.send({ message: "Express Server for Music System is running on Vercel" });
 });
 
 mongoose.connect(CONNECTION_URL)
-    .then(() => {
-        console.log("Database is connected successfully");
-        app.listen(PORT, () => {
-            console.log("Server is running on port 8000");
-        });
-    })
-    .catch((error) => {
-        console.error("Database connection error: ", error);
-    });
+    .then(() => console.log("Database is connected successfully"))
+    .catch((error) => console.error("Database connection error: ", error));
+
+module.exports = app;
